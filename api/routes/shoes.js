48773bb204119368591f2f5e1e33b6b333b54d41
analogy47 <actweed47@gmail.com>
2019-01-10 15:46:54 -0500

    add functionality to handle request body enable CORS change code style to single quotes

--- api/routes/shoes.js
+++ api/routes/shoes.js
@@ -1,33 +1,39 @@
-const express = require("express");
-const ShoeRouter = express.Router();
+const express = require('express')
+const ShoeRouter = express.Router()
 // bring in shoes controller
 
 // get all shoes
-ShoeRouter.get("/shoes", (req, res, next) => {
-	res.status(200).json({ message: "I can get all the shoes" });
-});
+ShoeRouter.get('/shoes', (req, res, next) => {
+  res.status(200).json({ message: 'I can get all the shoes' })
+})
 
 // add a new shoe
-ShoeRouter.post("/shoes", (req, res, next) => {
-	res.status(200).json({ message: "post shoes" });
-});
+ShoeRouter.post('/shoes', (req, res, next) => {
+  const shoe = {
+    shoeName: req.body.shoeName,
+    designer: req.body.designer,
+    price: req.body.price
+  }
+
+  res.status(201).json({ message: 'post shoes', shoe: shoe })
+})
 
 // display a single shoe
-ShoeRouter.get("/shoes/:shoeID", (req, res, next) => {
-	const id = req.params.shoeID;
-	res.status(200).json({ message: "get single shoe" });
-});
+ShoeRouter.get('/shoes/:shoeID', (req, res, next) => {
+  const id = req.params.shoeID
+  res.status(200).json({ message: 'get single shoe' })
+})
 
 // update a shoe
-ShoeRouter.put("/shoes/:shoeID", (req, res, next) => {
-	const id = req.params.shoeID;
-	res.status(200).json({ message: "update shoe" });
-});
+ShoeRouter.put('/shoes/:shoeID', (req, res, next) => {
+  const id = req.params.shoeID
+  res.status(200).json({ message: 'update shoe' })
+})
 
 // delete a shoe
-ShoeRouter.delete("/shoes/:shoeID", (req, res, next) => {
-	const id = req.params.shoeID;
-	res.status(200).json({ message: "delete shoe" });
-});
+ShoeRouter.delete('/shoes/:shoeID', (req, res, next) => {
+  const id = req.params.shoeID
+  res.status(200).json({ message: 'delete shoe' })
+})
 
-module.exports = ShoeRouter;
+module.exports = ShoeRouter
--- server.js
+++ server.js
@@ -1,47 +1,62 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const server = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const server = express()
 
-const port = process.env.Port || 5000;
+const port = process.env.Port || 5000
 
-//import routes
-const userRoutes = require("./api/routes/users");
-const shoeRoutes = require("./api/routes/shoes");
+// import routes
+const userRoutes = require('./api/routes/users')
+const shoeRoutes = require('./api/routes/shoes')
 
-server.use(morgan("dev"));
+server.use(morgan('dev'))
 
 // parse requests
-server.use(bodyParser.urlencoded({ extended: false }));
-server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }))
+server.use(bodyParser.json())
+
+// enable CORS
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+    return
+    res.status(200).json({})
+  }
+  return next()
+})
 
 // test route
-server.get("/", (req, res, next) => {
-	res.status(200).json({ api: "I can hear you Watson" });
-});
+server.get('/', (req, res, next) => {
+  res.status(200).json({ api: 'I can hear you Watson' })
+})
 
-server.use("/api", userRoutes);
-server.use("/api", shoeRoutes);
+server.use('/api', userRoutes)
+server.use('/api', shoeRoutes)
 
 // error handlers
 // 404 - not found
 server.use((req, res, next) => {
-	const error = new Error("Not Found");
-	error.status = 404;
-	next(error);
-});
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
 
 // general error handle to return any errors found
 server.use((error, req, res, next) => {
-	{
-		res.status(error.status || 500);
-		res.json({
-			error: { message: error.message }
-		});
-	}
-});
+  {
+    res.status(error.status || 500)
+    res.json({
+      error: { message: error.message }
+    })
+  }
+})
 
 // listen on port
 server.listen(port, () => {
-	console.log(`Server is listening on port ${port}`);
-});
+  console.log(`Server is listening on port ${port}`)
+})
