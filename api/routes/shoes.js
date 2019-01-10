0eadc6fde6909cfb52e766f0f0d61c1cffe8e2a0
analogy47 <actweed47@gmail.com>
2019-01-09 23:58:32 -0500

    add all routes for shoes and users

--- api/routes/shoes.js
+++ api/routes/shoes.js
@@ -7,4 +7,27 @@ ShoeRouter.get("/shoes", (req, res, next) => {
 	res.status(200).json({ message: "I can get all the shoes" });
 });
 
+// add a new shoe
+ShoeRouter.post("/shoes", (req, res, next) => {
+	res.status(200).json({ message: "post shoes" });
+});
+
+// display a single shoe
+ShoeRouter.get("/shoes/:shoeID", (req, res, next) => {
+	const id = req.params.shoeID;
+	res.status(200).json({ message: "get single shoe" });
+});
+
+// update a shoe
+ShoeRouter.put("/shoes/:shoeID", (req, res, next) => {
+	const id = req.params.shoeID;
+	res.status(200).json({ message: "update shoe" });
+});
+
+// delete a shoe
+ShoeRouter.put("/shoes/:shoeID", (req, res, next) => {
+	const id = req.params.shoeID;
+	res.status(200).json({ message: "delete shoe" });
+});
+
 module.exports = ShoeRouter;
