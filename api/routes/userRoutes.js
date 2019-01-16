36e186084c38f9092cd9698b1e4302308cdeb094
analogy47 <actweed47@gmail.com>
2019-01-16 02:54:07 -0500

    add all routes and controllers for users

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -5,28 +5,16 @@ const userController = require('../controllers/userController')
 // add a user
 userRouter.post('/users', userController.createUser)
 
-// TODO: reconfigure to use controller
 // get all users
-userRouter.get('/users', (req, res, next) => {
-  res.status(200).json({ message: 'I can get all the users' })
-})
+userRouter.get('/users', userController.getAllUsers)
 
 // display a single user
-userRouter.get('/users/:UsersID', (req, res, next) => {
-  const id = req.params.userID
-  res.status(200).json({ message: 'get single user', ID: id })
-})
+userRouter.get('/users/:id', userController.getOneUser)
 
 // update a user
-userRouter.put('/users/:userID', (req, res, next) => {
-  const id = req.params.userID
-  res.status(200).json({ message: 'update user' })
-})
+userRouter.put('/users/:id', userController.updateUser)
 
 // delete a user
-userRouter.delete('/users/:userID', (req, res, next) => {
-  const id = req.params.userID
-  res.status(200).json({ message: 'delete user' })
-})
+userRouter.delete('/users/:id', userController.removeUser)
 
 module.exports = userRouter
