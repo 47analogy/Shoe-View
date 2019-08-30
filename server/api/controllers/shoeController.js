008e671933dc9d3d613dcbf0a5c6bdac25348084
analogy47 <actweed47@gmail.com>
2019-08-30 11:54:31 -0400

    add cloudinary to store images

--- server/api/controllers/shoeController.js
+++ server/api/controllers/shoeController.js
@@ -1,121 +1,120 @@
 const Shoe = require('../models/shoeModel');
 const multer = require('multer');
 const path = require('path');
+const cloudinary = require('cloudinary');
+const cloudinaryStorage = require('multer-storage-cloudinary');
 
-const storage = multer.diskStorage({
-	destination: './public/uploads/',
-	filename: function(req, file, cb) {
-		cb(null, Date.now() + path.extname(file.originalname));
-	},
+// TODO: create config module
+const dotenv = require('dotenv');
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: 'shoes',
+  transformation: [{ width: 500, height: 500, crop: 'limit' }],
 });
 
 const upload = multer({ storage: storage }).single('image');
 
 // TODO: Validation
 exports.createShoe = (req, res, next) => {
-	upload(req, res, err => {
-		//   if (err instanceof multer.MulterError) {
-		//     return res.status(500).json(err)
-		//     res.send('wtf')
-		//     console.log('m1')
-		// } else if (err) {
-		//   res.send('humm')
-		//     console.log('m2')
-		// } else {
-		//   console.log('uploaded')
-		// }
+  upload(req, res, err => {
+    if (!req.file) {
+      res.status(400).send({
+        errorMessage: 'Error uploading image',
+      });
+    }
 
-		//console.log("Request file ---", req.file);
+    const shoe = new Shoe({
+      shoeName: req.body.shoeName,
+      designer: req.body.designer,
+      price: req.body.price,
+      image: Date.now() + path.extname(req.file.originalname),
+      //   path.extname(req.file.originalname).toString()
+    });
+    if (!shoe) {
+      res.status(400).send({
+        errorMessage: 'All fields are required',
+      });
+    }
+    console.log('Request file URL ---', req.file.url);
+    console.log('Request file ID ---', req.file.public_id);
 
-		if (!req.file) {
-			console.log(req);
-			console.log('Did not work');
-		} else {
-			console.log('req', req.file);
-		}
-
-		const shoe = new Shoe({
-			shoeName: req.body.shoeName,
-			designer: req.body.designer,
-			price: req.body.price,
-			image: req.file.filename,
-		});
-		if (!shoe) {
-			res.status(400).send({
-				errorMessage: 'All fields are required',
-			});
-		}
-		console.log('Request file ---', req.file);
-		// save shoe to database
-
-		shoe
-			.save()
-			.then(savedShoe => {
-				console.log(savedShoe);
-				res.status(201).json({ savedShoe: savedShoe });
-			})
-			.catch(err => {
-				//res.status(500).send({
-				//err: 'Shoe not saved to database'
-				console.log(err);
-			});
-	});
+    // save shoe to database
+    shoe
+      .save()
+      .then(savedShoe => {
+        console.log('DataBase', savedShoe);
+        res.status(201).json({ savedShoe: savedShoe });
+      })
+      .catch(err => {
+        //res.status(500).send({
+        //err: 'Shoe not saved to database'
+        console.log(err);
+      });
+  });
 };
 
 // get all the shoes
 exports.getAllShoes = (req, res, next) => {
-	Shoe.find()
-		.then(shoes => {
-			//console.log(shoes)
-			res.status(200).json({ shoes: shoes });
-		})
-		.catch(err => {
-			res.status(500).send({
-				error: 'Error getting all shoes from database',
-			});
-		});
+  Shoe.find()
+    .then(shoes => {
+      //console.log(shoes)
+      res.status(200).json({ shoes: shoes });
+    })
+    .catch(err => {
+      res.status(500).send({
+        error: 'Error getting all shoes from database',
+      });
+    });
 };
 
 // get single shoe  by id
 exports.getOneShoe = (req, res, next) => {
-	const shoeID = req.params.id;
-	Shoe.findById(shoeID)
-		.then(shoe => {
-			res.status(200).json({ shoe: shoe });
-		})
-		.catch(err => {
-			res.status(500).send({
-				error: 'Error getting this shoe from database',
-			});
-		});
+  const shoeID = req.params.id;
+  Shoe.findById(shoeID)
+    .then(shoe => {
+      res.status(200).json({ shoe: shoe });
+    })
+    .catch(err => {
+      res.status(500).send({
+        error: 'Error getting this shoe from database',
+      });
+    });
 };
 
 // find by id and update a shoe
 exports.updateShoe = (req, res, next) => {
-	const shoe = req.body;
-	const shoeID = req.params.id;
-	Shoe.findByIdAndUpdate(shoeID, shoe)
-		.then(updatedShoe => {
-			res.status(200).json({ updatedShoe: shoe });
-		})
-		.catch(err => {
-			res.status(500).send({
-				error: 'Shoe was not updated',
-			});
-		});
+  const shoe = req.body;
+  const shoeID = req.params.id;
+  Shoe.findByIdAndUpdate(shoeID, shoe)
+    .then(updatedShoe => {
+      res.status(200).json({ updatedShoe: shoe });
+    })
+    .catch(err => {
+      res.status(500).send({
+        error: 'Shoe was not updated',
+      });
+    });
 };
 
 // find by id and delete a shoe
 exports.removeShoe = (req, res, next) => {
-	const shoe = req.body;
-	const shoeID = req.params.id;
-	Shoe.findByIdAndRemove(shoeID, shoe)
-		.then(removedShoe => {
-			res.status(200).send(shoeID);
-		})
-		.catch(err => {
-			res.status(500).send({
-				error: 'Shoe was not deleted',
-			});
-		});
+  const shoe = req.body;
+  const shoeID = req.params.id;
+  Shoe.findByIdAndRemove(shoeID, shoe)
+    .then(removedShoe => {
+      res.status(200).send(shoeID);
+    })
+    .catch(err => {
+      res.status(500).send({
+        error: 'Shoe was not deleted',
+      });
+    });
 };
