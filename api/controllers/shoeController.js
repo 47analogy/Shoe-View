1cb43316e7d151e11b94e41b7df6505b881b0fde
analogy47 <actweed47@gmail.com>
2019-09-07 23:02:52 -0400

    add url for cloudinary

--- api/controllers/shoeController.js
+++ api/controllers/shoeController.js
@@ -11,6 +11,7 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  url: process.env.CLOUDINARY_URL, // deployment
 });
 
 const storage = cloudinaryStorage({
