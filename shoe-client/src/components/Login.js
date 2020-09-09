d0c3f0113f37c2e80afc48befca4fafaa8bad63c
analogy47 <actweed47@gmail.com>
2020-09-09 01:37:23 -0400

    add login form

--- shoe-client/src/components/Login.js
+++ shoe-client/src/components/Login.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Segment,
+  Container,
+} from 'semantic-ui-react';
 
 const Login = () => {
-  return <div>Login Page</div>;
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChange = (e) => {
+    setUserName(e.target.value);
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div style={{ marginTop: '400px' }}>
+      {/* <Container> */}
+      <Grid style={{ backgroundColor: '#36454f' }} centered columns={2}>
+        <Grid.Column>
+          <Header as="h2" textAlign="center"></Header>
+          <Segment>
+            <Form size="large">
+              <Form.Input
+                fluid
+                icon="user"
+                iconPosition="left"
+                placeholder="User Name"
+                onChange={(e) => onChange(e)}
+              />
+              <Form.Input
+                fluid
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                type="password"
+                onChange={(e) => onChange(e)}
+              />
+
+              <Button color="blue" fluid size="large">
+                Sign In
+              </Button>
+            </Form>
+          </Segment>
+        </Grid.Column>
+      </Grid>
+      {/* </Container> */}
+    </div>
+  );
 };
 
 export default Login;
